a5332bf0408071b4aa000f85551427a2ce02b1f1
AmiraslanSumgait <emiraslaneliyev45@gmail.com>
2021-10-06 03:57:07 +0400

    initial

---
+++ WinFormUniversityTask1/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace WinFormUniversityTask1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.btn_Reverse = new System.Windows.Forms.Button();
+            this.btn_Sort = new System.Windows.Forms.Button();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_Move = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(56, 40);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(133, 169);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(423, 40);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(133, 169);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 15;
+            this.listBox3.Location = new System.Drawing.Point(56, 368);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(133, 169);
+            this.listBox3.TabIndex = 2;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 15;
+            this.listBox4.Location = new System.Drawing.Point(423, 368);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(133, 169);
+            this.listBox4.TabIndex = 3;
+            // 
+            // btn_Reverse
+            // 
+            this.btn_Reverse.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_Reverse.Location = new System.Drawing.Point(249, 71);
+            this.btn_Reverse.Name = "btn_Reverse";
+            this.btn_Reverse.Size = new System.Drawing.Size(106, 41);
+            this.btn_Reverse.TabIndex = 4;
+            this.btn_Reverse.Text = "Reverse";
+            this.btn_Reverse.UseVisualStyleBackColor = true;
+            this.btn_Reverse.Click += new System.EventHandler(this.btn_Reverse_Click);
+            // 
+            // btn_Sort
+            // 
+            this.btn_Sort.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_Sort.Location = new System.Drawing.Point(249, 151);
+            this.btn_Sort.Name = "btn_Sort";
+            this.btn_Sort.Size = new System.Drawing.Size(106, 36);
+            this.btn_Sort.TabIndex = 5;
+            this.btn_Sort.Text = "Sort";
+            this.btn_Sort.UseVisualStyleBackColor = true;
+            this.btn_Sort.Click += new System.EventHandler(this.btn_Sort_Click);
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_Add.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_Add.Location = new System.Drawing.Point(249, 384);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(106, 41);
+            this.btn_Add.TabIndex = 6;
+            this.btn_Add.Text = "Add";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // btn_Move
+            // 
+            this.btn_Move.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_Move.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_Move.Location = new System.Drawing.Point(249, 496);
+            this.btn_Move.Name = "btn_Move";
+            this.btn_Move.Size = new System.Drawing.Size(106, 41);
+            this.btn_Move.TabIndex = 7;
+            this.btn_Move.Text = "Move";
+            this.btn_Move.UseVisualStyleBackColor = true;
+            this.btn_Move.Click += new System.EventHandler(this.btn_Move_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(255, 449);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(100, 23);
+            this.textBox.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 663);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.btn_Move);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.btn_Sort);
+            this.Controls.Add(this.btn_Reverse);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Button btn_Reverse;
+        private System.Windows.Forms.Button btn_Sort;
+        public System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Button btn_Move;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
---
+++ WinFormUniversityTask1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormUniversityTask1
+{
+    public partial class Form1 : Form
+    {
+        string[] stringArray = new string[] { "Kamal", "Akif", "Nebi", "Renat" };
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.DataSource = stringArray;
+        }
+        private void btn_Reverse_Click(object sender, EventArgs e)
+        {
+            string[] tempArray = new string[] { };
+            tempArray = stringArray;
+            Array.Reverse(tempArray);
+            listBox2.DataSource= tempArray;
+        }
+        private void btn_Sort_Click(object sender, EventArgs e)
+        {
+            string[] tempArray = new string[4];
+            for (int i = 0; i < stringArray.Count(); i++) tempArray[i] = stringArray[i];
+            Array.Sort<string>(tempArray);
+            listBox2.DataSource = tempArray;
+        }
+
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            if (textBox.Text != string.Empty)
+            {
+                listBox3.Items.Add(textBox.Text);
+                textBox.Text = string.Empty;
+            }
+            else MessageBox.Show("Text box is empty!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        private void btn_Move_Click(object sender, EventArgs e)
+        {
+            if (listBox3.SelectedIndex != -1)
+            {
+                int index = listBox3.SelectedIndex;
+                listBox4.Items.Add(listBox3.Items[index]);
+                listBox3.Items.RemoveAt(index);
+            }
+            else if (listBox3.Items.Count == 0) MessageBox.Show("List box is empty!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else MessageBox.Show("Please select an item!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
